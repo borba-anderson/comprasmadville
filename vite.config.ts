640bd59b44cfec02e2bc095c0ca756e9c189593b
Anderson Borba <166570725+borba-anderson@users.noreply.github.com>
2025-12-02 11:00:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
+ts
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/comprasmadville/",   // <-- ADICIONE ISSO AQUI
   server: {
     host: "::",
     port: 8080,
